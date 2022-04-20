80502f04e9720490e3b0f4ecb78056042a221174
Vitor <vitormuller66@gmail.com>
2022-04-20 18:11:03 -0300

    feat: initial setup backend

---
+++ src/Middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Response, Request } from "express";
+
+const errorTable = {
+    Bad_Request: 400,
+    Unauthorized: 401,
+    Unprocessable_Entity: 422,
+    Not_Found: 404,
+    Conflict: 409
+}
+
+export default function errorHandleMiddleware(err, req: Request, res: Response, next: NextFunction) {
+    if (!err.message) {
+        err.message === "An error as occured"
+    }
+    if (!errorTable[err.type]) {
+        return res.sendStatus(500)
+    }
+
+    return res.status(errorTable[err.type]).send(err.message);
+
+}
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { application, json } from "express"
+import "express-async-errors"
+import cors from "cors"
+import dotenv from "dotenv"
+import errorHandleMiddleware from "./Middlewares/errorHandlerMiddleware.js"
+import router from "./Router/index.js"
+
+
+
+const server = express()
+
+dotenv.config();
+
+server.use(cors());
+server.use(json());
+server.use(errorHandleMiddleware)
+server.use(router)
+
+
+server.listen(process.env.PORT || 4000, () => {
+    console.log("Running in 4000")
+})
